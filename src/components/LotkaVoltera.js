986424001a10fd67ae74bd4b33371b437d5857b1
khirshah <khirshah@gmail.com>
2020-03-18 22:34:30 +0000

    refactor: LotkaVoltera component

--- src/components/LotkaVoltera.js
+++ src/components/LotkaVoltera.js
@@ -42,31 +42,28 @@ export default class LotkaVoltera extends Component {
 
     //---------------------------------- vector data ---------------------------------
     this.vectorSpace = vectorSpace(this.xRange, this.yRange, this.xStep, this.yStep,this.modelParams);
-    //--------------------------------- trajectory ---------------------------------
 
+    //--------------------------------- trajectory ---------------------------------
     this.trajectory = trajectory(this.time,this.modelParams);
 
-    //------------------------------------ datascale -------------------------------------
-    const yScale =  d3.scaleLinear().domain(this.xRange).range([this.height - this.padding,this.padding]);
-    const xScale = d3.scaleLinear().domain(this.yRange).range([this.padding, this.width - this.padding]);
+    //---------------------------------- scale -------------------------------------
+    this.yScale =  d3.scaleLinear().domain(this.xRange).range([this.height - this.padding,this.padding]);
+    this.xScale = d3.scaleLinear().domain(this.yRange).range([this.padding, this.width - this.padding]);
+    this.xTicks = this.xScale.ticks(4);
+    this.yTicks = this.yScale.ticks(4);
 
-    //------------------------------------ color -----------------------------------------
+    //------------------------------------ color -----------------------------------
     const max_magnitude = this.vectorSpace.reduce(function (max_, it) {
       return (!isNaN(it.magnitude) && (max_ > it.magnitude)) ? max_ : it.magnitude;
     }, 0); 
 
-    const colorScale = d3.scaleSequential(d3.interpolateTurbo).domain([0,max_magnitude]);
+    this.colorScale = d3.scaleSequential(d3.interpolateTurbo).domain([0,max_magnitude]);
 
     //------------------------------------ STATE -----------------------------------------
     this.state = {
       modelParams: {...this.modelParams},
       trajectory: this.trajectory,
-      vectorSpace: [ ...this.vectorSpace ],   
-      xScale,
-      xTicks: xScale.ticks(4),
-      yScale,
-      yTicks: yScale.ticks(4),
-      colorScale
+      vectorSpace: [ ...this.vectorSpace ]
     };
   }
 
@@ -112,13 +109,13 @@ export default class LotkaVoltera extends Component {
     return vectorSpace.map((i,index) => {
       if (!isNaN(i.magnitude)) {
 
-        const nullX = this.state.xScale(0);
-        const nullY = this.state.yScale(0);
-        const x = this.state.xScale(i.x);
-        const y = this.state.yScale(i.y);
-        const modellX = this.state.xScale(i.modellValues[0]);
-        const modellY = this.state.yScale(i.modellValues[1]);
-        const magnitude = this.state.colorScale(i.magnitude)
+        const nullX = this.xScale(0);
+        const nullY = this.yScale(0);
+        const x = this.xScale(i.x);
+        const y = this.yScale(i.y);
+        const modellX = this.xScale(i.modellValues[0]);
+        const modellY = this.yScale(i.modellValues[1]);
+        const magnitude = this.colorScale(i.magnitude)
 
           return (
             <g key={index} >
@@ -147,8 +144,8 @@ export default class LotkaVoltera extends Component {
 
     const lineStatic = d3.line()
       .curve(d3.curveLinearClosed)
-      .x(d => this.state.xScale(d[0]))
-      .y(d => this.state.yScale(d[1]));
+      .x(d => this.xScale(d[0]))
+      .y(d => this.yScale(d[1]));
 
     return (     
       <Row>
@@ -171,30 +168,62 @@ export default class LotkaVoltera extends Component {
             <g>
               <path d={lineStatic(this.state.trajectory)} stroke="red" fill="none"></path>
             </g>
-            {/*-------------------------------- X Axis -----------------------*/}
+          <Axes properties={{
+            xScale: this.xScale,
+            yScale: this.yScale,
+            xTicks: this.xTicks,
+            yTicks: this.yTicks,
+            padding: this.padding,
+            margin: this.margin,
+            width: this.width,
+            height: this.height
+          }}/>
+          </svg>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+
+class Axes extends Component {
+  render() {
+      const {
+            xScale,
+            yScale,
+            xTicks,
+            yTicks,
+            padding,
+            margin,
+            width,
+            height
+            } = this.props.properties; 
+    return(
+          <g>
+           {/*-------------------------------- X Axis -----------------------*/}
             <g className={`chart-axis chart-axis--x ${styles.axis}`}
-               transform={`translate(0,${this.height-this.padding+5})`}>
+               transform={`translate(0,${height-padding+5})`}>
                 <line
-                  x1={this.padding}
-                  x2={this.width-this.padding}
+                  x1={padding}
+                  x2={width-padding}
                   y1="5"
                   y2="5"
                 > 
                 </line>
-              {this.state.xTicks.map((Xtick) => (
+              {xTicks.map((Xtick) => (
                   <text 
                     key={Xtick}
-                    x={this.state.xScale(Xtick)-10}
+                    x={xScale(Xtick)-10}
                     y="27"
                   >
                     {Xtick}
                   </text>
               ))}
-              {this.state.xTicks.map((Xtick) => (
+              {xTicks.map((Xtick) => (
                   <line
                     key={Xtick}
-                    x1={this.state.xScale(Xtick)}
-                    x2={this.state.xScale(Xtick)}
+                    x1={xScale(Xtick)}
+                    x2={xScale(Xtick)}
                     y1={5}
                     y2={12}
                   > 
@@ -204,10 +233,10 @@ export default class LotkaVoltera extends Component {
             {/*-------------------------------- Y Axis -----------------------*/}
               <g className={`chart-axis chart-axis--y  ${styles.axis}`}>
                 <line
-                  x1={this.padding-5}
-                  x2={this.padding-5}
-                  y1={this.padding}
-                  y2={this.width-this.padding}
+                  x1={padding-5}
+                  x2={padding-5}
+                  y1={padding}
+                  y2={width-padding}
                 > 
                 </line>
                 <text 
@@ -220,30 +249,28 @@ export default class LotkaVoltera extends Component {
 
                 </text>
 
-              {this.state.yTicks.map((Ytick) => (
+              {yTicks.map((Ytick) => (
                 <text 
                   className="tick"
                   key={Ytick}
                   x="12"
-                  y={this.state.yScale(Ytick) + 4}
+                  y={yScale(Ytick) + 4}
                 >
                   {Ytick}
                 </text>
               ))}
-              {this.state.yTicks.map((Ytick) => (
+              {yTicks.map((Ytick) => (
                 <line
                   key={Ytick}
-                  x1={this.padding-12}
-                  x2={this.padding-5}
-                  y1={this.state.yScale(Ytick)}
-                  y2={this.state.yScale(Ytick)}
+                  x1={padding-12}
+                  x2={padding-5}
+                  y1={yScale(Ytick)}
+                  y2={yScale(Ytick)}
                 > 
                 </line> 
               ))}
             </g>
-          </svg>
-        </Col>
-      </Row>
-    )
+        </g>
+      )
   }
 }
